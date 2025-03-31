6a8c2de79d6137a93f519b206dd89b087d0b195a
Anish <anishkumarak8686@gmail.com>
2025-03-31 12:04:58 +0530

    init

--- APP/BACKEND/Websocket/WebsocketHandler.js
+++ APP/BACKEND/Websocket/WebsocketHandler.js
@@ -1,11 +1,5 @@
 require('dotenv').config();
 const logger = require('../utils/logger');
-const validateHeaders = require('./validation/validateHeaders');
-const dbService = require('./services/dbService');
-const frameHandler = require("./constant");
-
-const HEARTBEAT_INTERVAL = process.env.HEARTBEAT_INTERVAL ? parseInt(process.env.HEARTBEAT_INTERVAL, 10) : 30000;
-const TIMEOUT_THRESHOLD = process.env.TIMEOUT_THRESHOLD ? parseInt(process.env.TIMEOUT_THRESHOLD, 10) : 60000;
 
 const handleWebSocketConnection = (
     WebSocket,
@@ -13,15 +7,7 @@ const handleWebSocketConnection = (
     ClientWss,
     wsConnections,
     ClientConnections,
-    clients,
-    OCPPResponseMap,
-    meterValuesMap,
-    sessionFlags,
-    charging_states,
-    startedChargingSet,
-    chargingSessionID,
-    chargerStartTime,
-    chargerStopTime
+    clients
 ) => {
     wss.on('connection', async (ws, req) => {
         ws.isAlive = true;
@@ -29,42 +15,6 @@ const handleWebSocketConnection = (
 
         ws.socket?.setNoDelay(true);
 
-        const uniqueIdentifier = await validateHeaders.getUniqueIdentifierFromRequest(req, ws);
-        if (!uniqueIdentifier) {
-            logger.loggerWarn('WebSocket connection established from browser');
-            return;
-        }
-
-        const previousResults = new Map();
-        const currentVal = new Map();
-        const clientIpAddress = req.connection.remoteAddress;
-
-        wsConnections.set(uniqueIdentifier, ws);
-        ClientConnections.add(ws);
-        clients.set(ws, uniqueIdentifier);
-
-        try {
-            await dbService.updateChargerIP(uniqueIdentifier, clientIpAddress);
-            await dbService.updateChargerStatus(uniqueIdentifier, clientIpAddress);
-            logger.loggerSuccess(`WebSocket connection established with Charger ID: ${uniqueIdentifier}`);
-        } catch (error) {
-            logger.loggerError(`Error updating charger details for ${uniqueIdentifier}: ${error.message}`);
-        }
-
-        // Heartbeat Monitoring
-        // Heartbeat Monitoring: Only send ping if the connection is not active
-        const heartbeatCheck = setInterval(() => {
-            const timeSinceLastHeartbeat = Date.now() - ws.lastHeartbeat;
-
-            if (timeSinceLastHeartbeat > TIMEOUT_THRESHOLD) {
-                logger.loggerWarn(`No response from ${uniqueIdentifier}, closing connection...`);
-                ws.terminate();
-                wsConnections.delete(uniqueIdentifier);
-            } else if (timeSinceLastHeartbeat > HEARTBEAT_INTERVAL) {
-                logger.loggerPingPong(`Sending ping to ${uniqueIdentifier}`);
-                ws.ping();
-            }
-        }, HEARTBEAT_INTERVAL);
 
         // Handle messages
         ws.on('message', (message) =>
@@ -74,20 +24,6 @@ const handleWebSocketConnection = (
                 ws,
                 WebSocket,
                 ClientWss,
-                currentVal,
-                previousResults,
-                wsConnections,
-                ClientConnections,
-                clients,
-                OCPPResponseMap,
-                meterValuesMap,
-                sessionFlags,
-                charging_states,
-                startedChargingSet,
-                chargingSessionID,
-                chargerStartTime,
-                chargerStopTime,
-                clientIpAddress
             )
         );
 
@@ -149,55 +85,6 @@ const handleIncomingMessage = async (
     try {
         const requestData = JSON.parse(message);
         logger.loggerDebug(`Received message from ${uniqueIdentifier}: ${message}`);
-
-        if (!Array.isArray(requestData) || requestData.length < 4) {
-            return ws.send(JSON.stringify([3, requestData[1], { status: "Rejected", errors: ["Invalid message format"] }]));
-        }
-
-        const requestType = requestData[0];
-        const requestId = requestData[1];
-        const requestName = requestData[2];
-        const requestPayload = requestData[3];
-
-        let response = [3, requestId, {}];
-        let errors = [];
-
-        switch (requestName) {
-            case "DataTransfer":
-                errors = frameHandler.handleDataTransfer(requestPayload, requestId);
-                break;
-
-            case "FirmwareStatusNotification":
-                errors = frameHandler.handleFirmwareStatusNotification(requestPayload, requestId);
-                break;
-
-            case "BootNotification":
-                response = await frameHandler.handleBootNotification(uniqueIdentifier, requestPayload, requestId);
-                break;
-
-            case "Heartbeat":
-                response = await frameHandler.handleHeartbeat(uniqueIdentifier, requestPayload, requestId, currentVal, previousResults, ws);
-                break;
-
-            case "StatusNotification":
-                response = await frameHandler.handleStatusNotification(uniqueIdentifier, requestPayload, requestId, sessionFlags, startedChargingSet, charging_states, chargingSessionID, chargerStartTime, chargerStopTime, meterValuesMap, clientIpAddress);
-                break;
-
-            case "Authorize":
-                response = await frameHandler.handleAuthorize(uniqueIdentifier, requestPayload, requestId);
-                break;
-
-            // case "StartTransaction":
-            //     response = await frameHandler.handleStartTransaction(uniqueIdentifier, requestPayload, requestId);
-            //     break;
-
-        }
-
-        if (errors.length > 0) {
-            return ws.send(JSON.stringify([3, requestId, { status: "Rejected", errors }]));
-        }
-
-        ws.send(JSON.stringify(response));
 
         // Broadcast message to other clients
         broadcastMessage(uniqueIdentifier, requestData, ws, ClientWss);
---
+++ APP/BACKEND/middlewares/emailer.js
@@ -0,0 +1,151 @@
+const nodemailer = require('nodemailer');
+const logger = require('../utils/logger');
+
+// Create a transporter object
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // Gmail SMTP server
+    port: 465, // 465 for SSL or 587 for TLS
+    secure: true, // true for SSL
+    auth: {
+        user: "", // Your Gmail email address
+        pass: "", // App Password (not your regular Gmail password)
+    },
+});
+// Function to send email
+const sendEmail = async (to, subject, text) => {
+    try {
+        // Define email options
+        let info = await transporter.sendMail({
+            from: '"LearnPro AI" <anish@zdart.tech>', // sender address
+            to: to, // list of receivers
+            subject: subject, // Subject line
+            text: text, // plain text body
+            html: `<p>${text}</p>`, // HTML body
+        });
+
+        logger.loggerSuccess(`Message sent: ${info.messageId}`);
+        console
+        return true;
+    } catch (error) {
+        logger.loggerError(`Error sending email: ${error}`);
+        return false;
+    }
+}
+const EmailConfig = async (email, mailHead, otp) => {
+    try {
+        let sendTo = email;
+        let mail_subject;
+        let mail_body;
+        if (mailHead === 'OTP') {
+            mail_subject = 'LearnPro AI - OTP';
+            mail_body = `
+                <html>
+                    <head>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                                margin: 0;
+                                padding: 20px;
+                                background-color: #f4f4f4;
+                            }
+                            .container {
+                                background-color: #ffffff;
+                                border-radius: 8px;
+                                padding: 20px;
+                                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                            }
+                            h2 {
+                                color: #333;
+                            }
+                            p {
+                                color: #555;
+                                line-height: 1.6;
+                            }
+                            .otp {
+                                font-weight: bold;
+                                font-size: 1.2em;
+                                color: #007BFF; /* Bootstrap primary color */
+                                background-color: #e9f5ff; /* Light background for the OTP */
+                                padding: 10px;
+                                border-radius: 4px;
+                                display: inline-block;
+                            }
+                            .footer {
+                                margin-top: 20px;
+                                font-size: 0.9em;
+                                color: #888;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div class="container">
+                            <h2>Hello ${email},</h2>
+                            <p>We received a request to log in or register with your account. Please use the following One-Time Password (OTP) to proceed with the process:</p>
+                            <p>Your OTP is: <span class="otp">${otp}</span></p>
+                            <p class="footer">Thank you,<br>EV POWER</p>
+                        </div>
+                    </body>
+                </html>
+            `;
+        } else if (mailHead === 'deleteAccount') {
+            mail_subject = 'LearnPro AI - account deletion'
+            mail_body = `
+                <html>
+                    <head>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                                margin: 0;
+                                padding: 20px;
+                                background-color: #f4f4f4;
+                            }
+                            .container {
+                                background-color: #ffffff;
+                                border-radius: 8px;
+                                padding: 20px;
+                                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+                            }
+                            h2 {
+                                color: #333;
+                            }
+                            p {
+                                color: #555;
+                                line-height: 1.6;
+                            }
+                            .otp {
+                                font-weight: bold;
+                                font-size: 1.2em;
+                                color: #007BFF; /* Bootstrap primary color */
+                                background-color: #e9f5ff; /* Light background for the OTP */
+                                padding: 10px;
+                                border-radius: 4px;
+                                display: inline-block;
+                            }
+                            .footer {
+                                margin-top: 20px;
+                                font-size: 0.9em;
+                                color: #888;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div class="container">
+                            <h2>Hello ${email},</h2>
+                            <p>Your Ion Hive account is deleted successfully !</p>
+                            <p class="footer">Thank you,<br>Ion Hive</p>
+                        </div>
+                    </body>
+                </html>
+            `;
+        }
+
+        const result = await sendEmail(sendTo, mail_subject, mail_body);
+        return result;
+    } catch (error) {
+        logger.loggerError(`Error sending email: ${error}`);
+        return false;
+    }
+}
+
+
+module.exports = { EmailConfig }
